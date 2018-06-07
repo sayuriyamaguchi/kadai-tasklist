a466c739d586baa7e8bea0b35f53ec1257de2bbe
sayuri.yamaguchi <sayuri.a.yamaguchi@rakuten.com>
2018-06-07 03:57:04 +0000

    bootstrap design

--- resources/views/tasks/edit.blade.php
+++ resources/views/tasks/edit.blade.php
@@ -4,17 +4,27 @@
 
     <h1>id: {{ $task->id }} のタスク編集ページ</h1>
 
-    {!! Form::model($task, ['route' => ['tasks.update', $task->id], 'method' => 'put']) !!}
+
+    <div class="row">
+         <div class="col-xs-12 col-sm-offset-2 col-sm-8 col-lg-offset-3 col-lg-6">
+             
+                {!! Form::model($task, ['route' => ['tasks.update', $task->id], 'method' => 'put']) !!}
+        
+        <div class="form-group">
+                {!! Form::label('status', 'ステータス:') !!}
+                {!! Form::text('status',  null, ['class' => 'form-control']) !!}
+        </div>
+        
+        <div class="form-group">
+                {!! Form::label('content', 'タスク:') !!}
+                {!! Form::text('content', null, ['class' => 'form-control']) !!}
+            </div>
 
 
-        {!! Form::label('status', 'ステータス:') !!}
-        {!! Form::text('status') !!}
-
-        {!! Form::label('content', 'メッセージ:') !!}
-        {!! Form::text('content') !!}
-
-        {!! Form::submit('更新') !!}
+                {!! Form::submit('更新', ['class' => 'btn btn-default']) !!}
 
     {!! Form::close() !!}
-
+        </div>
+    </div>
+    
 @endsection
